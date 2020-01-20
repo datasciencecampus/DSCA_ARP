4e5487b59e8a9f3fb6903434f24dddbd9a71ea45
Sonja <38615988+sonjam111@users.noreply.github.com>
2020-01-20 17:55:27 +0000

    Delete render_report.R

--- render_report.R
+++
@@ -1,6 +0,0 @@
-render_report <- function(continent) {
-  rmarkdown::render("hiv_report.Rmd", 
-                    output_file = paste0(continent, "_", year, "_report_", ".html"),
-                    params = list(current_continent = continent), 
-                    output_options = list(self_contained = FALSE, lib_dir = "libs"))
-}
