7147816de3171d031903c9c6a257c8631d53c609
Sonja <38615988+sonjam111@users.noreply.github.com>
2020-01-30 09:57:22 +0000

    Add files via upload

--- render_report.R
+++ render_report.R
@@ -3,7 +3,6 @@
 render_report <- function(continent, YEAR, dataFile) {
   rmarkdown::render("hiv_report.Rmd", 
                     output_file = paste0(continent, "_", YEAR, "_report", ".html"),
-                    #params = list(current_continent = continent, year = YEAR, data_file = dataFile), 
                     output_options = list(self_contained = FALSE, lib_dir = "libs"),
                     params = "ask")
 }
